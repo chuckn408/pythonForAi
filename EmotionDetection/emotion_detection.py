1e74ddc66b57fe29a17bfa85f84c6699577b7f1c
chuckn408 <nelsoch@pm.me>
2024-03-25 02:50:38 -0400

    nearly done

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,10 +7,14 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     #myobj = { "raw_document": { "text": "Testing this application for error handling" } }
+
     response = requests.post(url, json = myobj, headers=header)
-    print(response.status_code)
-    formatted_response = json.loads(response)
-    emotion = formatted_response['emotionPredictions']['emotion']
-    score = formatted_response['emotionPredictions']['score']
+    print("status: ",response.status_code)
+
+    formatted_response = json.loads(response.text)
+    #emotion = formatted_response['emotionPredictions']['emotion']
+    #score = formatted_response['emotionPredictions']['score']
+
+    print(formatted_response)
     #return {formatted_response}
-    return {emotion, score}
+    #return {emotion, score}
